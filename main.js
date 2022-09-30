ad210cd5b41325f9791e22b076b017bcf9f20dfd
Andrew Brantley <brantley@us.ibm.com>
2022-09-30 11:40:37 -0500

    start work with graphql

--- main.js
+++ main.js
@@ -12,6 +12,11 @@ const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+const { GraphQLJSON, GraphQLJSONObject } = require('graphql-type-json');
+const { json } = require('express/lib/response');
+
 function getDb(db_name){
     let db = nano.db.use(db_name);
     return db;
@@ -23,23 +28,80 @@ app.set("port", process.argv[2] || 3000);
 app.use(bodyParser.json());
 
 app.get(['/','/data'],async(req, res, next)=>{
-
+    
+    console.log('req start');
+    
     const db = getDb(db_name);
+    
+    console.log('req 2');
 
-    // returns the first 25, unless noted to be different 
-    content = await db.find({
-        "selector": {
-           "_id": {
-              "$gt": null
-           }
-        }
-    });
+    const content = await getData(db);
 
     console.log('req');
 
     res.json(content.docs);
 });
 
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  scalar JSON
+
+  type Query {
+    hello: String,
+    count: Int,
+    docs: JSON,    
+  }
+
+`);
+
+// The root provides a resolver function for each API endpoint
+var str_to_send = 'Hello world!!!';
+var root = {
+  JSON: GraphQLJSON, 
+
+  hello: async(...a) => {    
+    const content = await getData(db);
+    console.log(`content is `);
+    // console.log(content);
+    return content;
+  },
+
+  count:async() => {
+    return str_to_send.length
+  },
+
+  docs: async()=>{
+    return (await getData(getDb(db_name))).docs;
+  }
+};
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
 app.listen(app.get('port'),()=>{
 	console.log("listening on port "+app.get('port'));
 });
+
+async function getData(db){
+
+  // returns the first 25, unless noted to be different 
+  return await db.find({
+    "selector": {
+       "_id": {
+          "$gt": null
+       }
+    }
+  });
+
+}
+
+// TODO remove
+function timeoutPromise(ms){
+  return new Promise((resolve, reject)=>{
+    setTimeout(resolve,ms);
+  }); 
+}
+
